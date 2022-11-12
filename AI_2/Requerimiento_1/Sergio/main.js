3ba72ee04c5e51ab0eeff5b103ddb4b3e5f47be0
sergio4884 <sergio4884mr@gmail.com>
2022-11-12 16:29:51 +0100

    mejoras

--- AI_2/Requerimiento_1/Sergio/main.js
+++ AI_2/Requerimiento_1/Sergio/main.js
@@ -1,34 +1,51 @@
-const URL_DESTINO = "http://localhost:5500/AI_2/Requerimiento_1/Sergio/"
+const URL_DESTINO = "http://localhost:5500/WsJavaScript/0000pruebas/"
 const RECURSO1 = "tamaños.json"
 const RECURSO2 = "ingredientes.json"
 
 
-    
-    function crearRadio(texto){
-        var radioA= document.createElement("input")
+    function crearTitulo(texto){
+        var elemento = document.createElement("h2")
+        var text = document.createTextNode(texto)
+        elemento.appendChild(text)
+        formulario.appendChild(elemento)
+    }
+    function crearRadio(precio, texto){
+        let radioA= document.createElement("input")
+        let prec = document.createAttribute("value")
         radioA.type="radio"
         radioA.name= "tamaño"
+        
+        prec.value=precio
+        radioA.setAttributeNode(prec)
         formulario.appendChild(radioA)
-        var etiqueta = document.createElement('label')
-        var contenido = document.createTextNode(texto)
+
+        let etiqueta = document.createElement('label')
+        let contenido = document.createTextNode(texto)
         etiqueta.appendChild(contenido)
         formulario.appendChild(etiqueta)
-        var espacio = document.createElement("br")
+        let espacio = document.createElement("br")
         formulario.appendChild(espacio)
         
     }
 
-    function crearCheckbox(ingrediente, texto){
+    function crearCheckbox(ingrediente, texto, contenido){
         var check = document.createElement("input")
+        var valor = document.createAttribute("value")
         check.type="checkbox"
         check.name=ingrediente
+        valor.value=texto
+        check.setAttributeNode(valor)
         formulario.appendChild(check)
+
+        
         var etiqueta = document.createElement('label')
-        var contenido = document.createTextNode(texto)
+        var contenido = document.createTextNode(contenido)
         etiqueta.appendChild(contenido)
         formulario.appendChild(etiqueta)
         var espacio=document.createElement("br")
         formulario.appendChild(espacio)
+        
+       
     }
 
     function peticionTamaños() {
@@ -38,7 +55,8 @@ const RECURSO2 = "ingredientes.json"
         xmlHttp.onreadystatechange = function () {
             if (this.readyState == 4) {
                 if (this.status == 200) {
-                    procesarRespuestaTamaños(this.responseText)//Obtenemos el valor en texto
+                    procesarRespuestaTamaños(this.responseText)
+                    
                 } else {
                     alert("ZASCA!")
                 }
@@ -56,7 +74,7 @@ const RECURSO2 = "ingredientes.json"
         xmlHttp.onreadystatechange = function () {
             if (this.readyState == 4) {
                 if (this.status == 200) {
-                    procesarRespuestaIngredientes(this.responseText)//Obtenemos el valor en texto
+                    procesarRespuestaIngredientes(this.responseText)                    
                 } else {
                     alert("ZASCA!")
                 }
@@ -70,50 +88,46 @@ const RECURSO2 = "ingredientes.json"
 
 
     function procesarRespuestaTamaños(jsonDoc){
-        //Convertimos un texto a un objeto JSON
+        
         var objetoJson = JSON.parse(jsonDoc)
-        //Podemos hacer lo contrario con "JSON.stringify(obj)"
+        
         console.log(objetoJson)
 
-        //Lo suyo seria crear objetos con el DOM, esto esta regulín
-        //pero como ejemplo sencillo vale
-        //crearRadio("Grande")
-        var arrayTamanos = objetoJson.PIZZERIA.TAMAÑOS;//Ojo mayusculas y minusculas, es como este en el json original
         
-        //Iteramos el array de CDs y formamos las filas y columnas
+        var arrayTamanos = objetoJson.PIZZERIA.TAMAÑOS;
+        crearTitulo("ESTOS SON NUESTROS TAMAÑOS")
+       
         for (let i of arrayTamanos) {
             
-            crearRadio( i.TAMAÑO +" " +i.PRECIO)
+            crearRadio( i.PRECIO, i.TAMAÑO +" " +i.PRECIO, i.TAMAÑO)
             let bot = document.getElementById("tam")
             bot.setAttribute("disabled", "")
             
         }
         resultadoBusqueda.innerHTML = table
+        
     }
     
     function procesarRespuestaIngredientes(jsonDoc){
-        //Convertimos un texto a un objeto JSON
+        
         var objetoJson = JSON.parse(jsonDoc)
-        //Podemos hacer lo contrario con "JSON.stringify(obj)"
+        
         console.log(objetoJson)
 
-        //Lo suyo seria crear objetos con el DOM, esto esta regulín
-        //pero como ejemplo sencillo vale
-        //crearRadio("Grande")
-        var arrayTamanos = objetoJson.PIZZA.INGREDIENTES;//Ojo mayusculas y minusculas, es como este en el json original
         
-        //Iteramos el array de CDs y formamos las filas y columnas
+        var arrayTamanos = objetoJson.PIZZA.INGREDIENTES;
+        crearTitulo("ESTOS SON NUESTROS INGREDIENTES")
+        
         for (let i of arrayTamanos) {
             
-            crearCheckbox("INGREDIENTE", i.INGREDIENTE +" " +i.PRECIO)
+            crearCheckbox( i.INGREDIENTE ,i.PRECIO, i.INGREDIENTE+" "+i.PRECIO)
             let bot = document.getElementById("ingre")
             bot.setAttribute("disabled", "")
             
         }
         resultadoBusqueda2.innerHTML = table
     }
-
-
+ 
 
 
 
@@ -122,6 +136,8 @@ const RECURSO2 = "ingredientes.json"
         
         tam.onclick=peticionTamaños
         ingre.onclick=peticionIngredientes
+
+        
     }
 
     
